9538f77166674db67726d043e14a66e421a872db
Oladimeji Seunayo Ezekiel <oladimejiseunayo@gmail.com>
2023-12-14 02:56:45 -0800

    corrections

--- database/index.js
+++ database/index.js
@@ -1,11 +1,22 @@
 const mysql = require("mysql");
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
     host: process.env.MYSQLHOST, 
     user: process.env.MYSQLUSER, 
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
  
 });
 
+db.getConnection((err, conn) => {
+    if(err) console.log(err)
+    console.log("Connected successfully")
+})
+
 module.exports = db.promise()
+
+
+

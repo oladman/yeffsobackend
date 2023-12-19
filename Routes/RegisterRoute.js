e1d11accd5ab1e9386e356c82b778b3b952ee519
Oladimeji Seunayo Ezekiel <oladimejiseunayo@gmail.com>
2023-12-19 14:21:05 -0800

    hi

--- Routes/RegisterRoute.js
+++ Routes/RegisterRoute.js
@@ -3,7 +3,6 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-require('dotenv').config()
 
 const app = express();
 const router = express();
@@ -13,26 +12,25 @@ app.use(cors({credentials: true, origin: 'https://yeffso.netlify.app'}));
 
 
 router.get("/", (req, res) => res.send("Main Register Page"));
-
-
 router.post("/", (req, res) => {
   const sql =
-  "INSERT INTO users (`FirstName`, `LastName`, `Email`, `PhoneNumber`, `Password`, `Role`) VALUES (?)";
-  const RegValue = [
-    req.body.firstname,
-    req.body.lastname,
-    req.body.email,
-    req.body.phonenumber,
-    req.body.password,
-    req.body.role,
-  ];
-  db.query(sql, [RegValue], (err, result) => {
-    console.log(RegValue)
-
-    if (err) return res.json({ Error: "Error inserting into server" });
-    return res.json({ Status: "Success" });
+    "INSERT INTO users (`FirstName`, `LastName`, `Email`, `PhoneNumber`, `Password`, `Role`) VALUES (?)";
+  bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+    if (err) return res.json({ Error: "Error harshing password" });
+    const RegValue = [
+      req.body.firstname,
+      req.body.lastname,
+      req.body.email,
+      req.body.phonenumber,
+      hash,
+      req.body.role,
+    ];
+    db.query(sql, [RegValue], (err, result) => {
+      console.log(RegValue)
+      if (err) return res.json({ Error: "Error inserting into server" });
+      return res.json({ Status: "Success" });
+    });
   });
 });
-
 
 module.exports = router;

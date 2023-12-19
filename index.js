633740056161c624fd84f55ef963ff0233ed8694
Oladimeji Seunayo Ezekiel <oladimejiseunayo@gmail.com>
2023-12-19 13:15:41 -0800

    hmm

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const verifyUser = (req, res, next) => {
 };
 
 
-app.get("/", (req, res) => {
+app.get("/", verifyUser, (req, res) => {
   return res.json({ Status: "Success", Email: req.Email, Role: req.Role , id: req.id, FirstName: req.FirstName });
 });
 

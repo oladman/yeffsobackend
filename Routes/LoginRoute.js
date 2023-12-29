1dcb29bf193b850f6989c69217a5ac36a5647df8
Oladimeji Seunayo Ezekiel <oladimejiseunayo@gmail.com>
2023-12-29 14:06:42 -0800

    upgrade

--- Routes/LoginRoute.js
+++ Routes/LoginRoute.js
@@ -10,6 +10,9 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({credentials: true, origin: 'https://yeffso.netlify.app'}));
 
 
